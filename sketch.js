8c8c8d9ec9207cdbc96eeb6f0fd992f3490dd9c0
Víctor Manuel Bátiz Beltrán <51245128+vbatiz@users.noreply.github.com>
2025-03-24 23:04:51 +0000

    Se terminó programa

--- sketch.js
+++ sketch.js
@@ -1,17 +1,83 @@
+let tierra
+
 function preload() {
   // put preload code here
+  tierra = loadImage('./images/tierra.jpg')
 }
 
 function setup() {
   // put setup code here
-  createCanvas(800,800, WEBGL)
+  createCanvas(windowWidth,windowHeight, WEBGL)
 }
 
 function draw() {
   // put drawing code here
-  background(255)
+  background(0)
+  //rotateX(frameCount * 0.01)
+  //rotateY(frameCount * 0.01)
+  //rotateZ(frameCount * 0.01)
+  //box(150)
+  translate(-450,-200,0)
+  push()
   rotateX(frameCount * 0.01)
   rotateY(frameCount * 0.01)
   rotateZ(frameCount * 0.01)
-  box(150)
+  texture(tierra)
+  plane(160,60,60)
+  pop()
+ 
+  translate(250,0,0)
+  push()
+  rotateX(frameCount * 0.01)
+  rotateY(frameCount * 0.01)
+  rotateZ(frameCount * 0.01)
+  texture(tierra)
+  sphere(80,30)
+  pop()
+ 
+  translate(250,0,0)
+  push()
+  rotateX(frameCount * 0.01)
+  rotateY(frameCount * 0.01)
+  rotateZ(frameCount * 0.01)
+  texture(tierra)
+  torus(60,40,30)
+  pop()
+
+  translate(250,0,0)
+  push()
+  rotateX(frameCount * 0.01)
+  rotateY(frameCount * 0.01)
+  rotateZ(frameCount * 0.01)
+  texture(tierra)
+  cone(80,80,30)
+  pop()
+
+  translate(-750,250,0)
+  push()
+  rotateX(frameCount * 0.01)
+  rotateY(frameCount * 0.01)
+  rotateZ(frameCount * 0.01)
+  texture(tierra)
+  box(100)
+  pop()
+
+  translate(250,0,0)
+  push()
+  rotateX(frameCount * 0.01)
+  rotateY(frameCount * 0.01)
+  rotateZ(frameCount * 0.01)
+  texture(tierra)
+  cylinder(80,120,30)
+  pop()
+
+  translate(250,0,0)
+  push()
+  rotateX(frameCount * 0.01)
+  rotateY(frameCount * 0.01)
+  rotateZ(frameCount * 0.01)
+  texture(tierra)
+  ellipsoid(80,60,40,30)
+  pop()
+
 }
